22e46c081060234e1c19a4dc7c832672f0fb5775
Yutong <69740611+Yutong-gannis@users.noreply.github.com>
2023-07-04 18:39:52 +0800

    Delete ModelPlan directory

--- ModelPlan/Postprocess/Poseprocess.py
+++
@@ -1,6 +0,0 @@
-class Pose:
-    def __init__(self, line):
-        self.velocity_mean = line[:3]
-        self.rotation_mean = line[3:6]
-        self.velocity_std = line[6:9]
-        self.rotation_std = line[9:12]
