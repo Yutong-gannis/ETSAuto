774d3a8899025762533bb83088d79086d9fdf771
Yutong <69740611+Yutong-gannis@users.noreply.github.com>
2023-08-29 01:14:14 +0800

    Delete ModelPlan directory

--- ModelPlan/load_model.py
+++
@@ -1,7 +0,0 @@
-from ModelPlan.Planner_onnx import Supercombo_onnx_1, Supercombo_onnx_2, Supercombo_onnx_3
-from ModelPlan.Planner_trt import Supercombo_trt_1, Supercombo_trt_2, Supercombo_trt_3
-
-
-def load_model(supercombo_path, version, engine):
-    supercombo = eval('Supercombo_' + engine + '_' + str(version))(supercombo_path)
-    return supercombo
