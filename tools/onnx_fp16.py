626b8ed17b89de12122b32bf4812342b250c636e
Yutong <69740611+Yutong-gannis@users.noreply.github.com>
2023-08-29 01:15:26 +0800

    Delete tools directory

--- tools/onnx_fp16.py
+++
@@ -1,7 +0,0 @@
-from onnx import load_model, save_model
-from onnxmltools.utils import float16_converter
-
-output_onnx_name = '../weights/supercombo_2.onnx'
-onnx_model = load_model(output_onnx_name)
-trans_model = float16_converter.convert_float_to_float16(onnx_model)
-save_model(trans_model, "../weights/supercombo_2_fp16.onnx")
