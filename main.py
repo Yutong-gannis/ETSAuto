b885a7a497d01395be3d9faf2196012c764eb520
Yutong <69740611+Yutong-gannis@users.noreply.github.com>
2023-09-01 21:03:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from Navigation.Navigation_Process import nav_process
 from Common.iodata import load_pkl, save_pkl
 
 nav_line = None
-lane_path = os.path.abspath(os.path.join(project_path, 'weights/bevlanedet/resnet18_0.5_new/ep049.onnx'))
+lane_path = os.path.abspath(os.path.join(project_path, 'weights/ep049.onnx'))
 lanedet = Bev_Lanedet(lane_path)
 screengraber = ScreenGraber()
 fps = 0.06
