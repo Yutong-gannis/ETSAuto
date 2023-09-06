03045cfb1db2c9e3d9da30810a9acddf59ab886b
Yutong <69740611+Yutong-gannis@users.noreply.github.com>
2023-09-06 14:08:04 +0800

    Update ETSAuto.bat

--- ETSAuto.bat
+++ ETSAuto.bat
@@ -2,5 +2,5 @@
 call conda activate
 start python User/user_launcher.py
 start python Perception/perception_launcher.py
-start python Condition/condition_launcher.py
 start python Control/control_launcher.py
+start python Condition/condition_launcher.py
