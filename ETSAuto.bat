66a05e0faa5d17eb5500129d86640eec8fd92cb4
Yutong <69740611+Yutong-gannis@users.noreply.github.com>
2023-09-07 20:43:16 +0800

    Delete ETSAuto.bat

--- ETSAuto.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call conda activate
-start python User/user_launcher.py
-start python Perception/perception_launcher.py
-start python Control/control_launcher.py
-start python Condition/condition_launcher.py
