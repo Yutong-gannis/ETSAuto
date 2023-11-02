74823d3fca834d83a01842b4e16217b46caf4779
Yutong <69740611+Yutong-gannis@users.noreply.github.com>
2023-11-02 12:20:07 +0800

    Update condition_launcher.py

--- Selfdrive/Condition/condition_launcher.py
+++ Selfdrive/Condition/condition_launcher.py
@@ -1,6 +1,6 @@
 import time
 import os
-
+import sys
 from loguru import logger
 from shared_memory_dict import SharedMemoryDict
 
@@ -9,6 +9,7 @@ from hardware_condition import Hardware
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 project_path = os.path.abspath(os.path.join(current_path, '../..'))
+sys.path.append(project_path)
 from Common.log import condition_data_level, condition_info_level, condition_warning_level
 
 logger.add(os.path.join(project_path, "log/run.log"), rotation="100 MB")
